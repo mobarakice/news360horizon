d2aedb9031adbbe1679f7a83fd2f94ecdb614d28
mobarak <mobarak.ice.06@gmail.com>
2023-09-30 21:55:27 +0600

    Project init done

---
+++ src/main/kotlin/com/news360horizon/news360horizon/News360horizonApplication.kt
@@ -0,0 +1,11 @@
+package com.news360horizon.news360horizon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class News360horizonApplication
+
+fun main(args: Array<String>) {
+	runApplication<News360horizonApplication>(*args)
+}
---
+++ src/test/kotlin/com/news360horizon/news360horizon/News360horizonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.news360horizon.news360horizon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class News360horizonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
